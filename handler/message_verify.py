f44df4108096e19f3768950d6cdb88466f216fd8
Akbar <akbarjonjorayev001@gmail.com>
2024-12-20 18:52:51 +0500

    no excess asking for phone number

--- handler/message_verify.py
+++ handler/message_verify.py
@@ -1,8 +1,19 @@
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
+import tg_bot_utils
+import user
 
 
 async def verify(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    phone_number = user.user_data["phone_number"]
+
+    if phone_number:
+        await tg_bot_utils.tpo_website_buttons(phone_number, update, context)
+    else:
+        await ask_for_contact(update, context)
+
+
+async def ask_for_contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[KeyboardButton(text="Share contact", request_contact=True)]]
     reply_markup = ReplyKeyboardMarkup(
         keyboard, one_time_keyboard=True, resize_keyboard=True
