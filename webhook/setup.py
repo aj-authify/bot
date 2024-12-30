08bd3d6dceb429041071c04c04bf10dfbcd125ce
Akbar <akbarjonjorayev001@gmail.com>
2024-12-30 12:17:20 +0500

    response fixed

--- webhook/setup.py
+++ webhook/setup.py
@@ -10,9 +10,7 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 def set_webhook():
     webhook_url = f"{WEBHOOK_URL}/webhook"
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook"
-    data = {
-        "url": webhook_url,
-    }
+    data = {"url": webhook_url, "drop_pending_updates": True}
     response = requests.post(url, json=data)
 
     if response.status_code == 200:
